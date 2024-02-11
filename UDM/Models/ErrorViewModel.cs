3269d1e734723cdbea9441e7be4b51a84d634789
ApachUA <4971779@gmail.com>
2024-02-11 16:43:52 +0200

    Добавьте файлы проекта.

---
+++ UDM/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UDM.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
