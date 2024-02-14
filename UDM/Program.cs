ee5879093db0500600f8965c6cfaee34d52f6231
ApachUA <4971779@gmail.com>
2024-02-14 12:57:49 +0200

    Services registered

--- UDM/Program.cs
+++ UDM/Program.cs
@@ -1,15 +1,26 @@
+using UDM.Services.Implementations;
+using UDM.Services.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddScoped<IPersonService, PersonService>();
+builder.Services.AddScoped<IAddressService, AddressService>();
+builder.Services.AddScoped<IEmployeeService, EmployeeService>();
+builder.Services.AddScoped<IContactService, ContactService>();
+
+builder.Services.AddScoped<IPersonTransactionService, PersonTransactionService>();
+
+builder.Services.AddScoped<IStaffService, StaffService>();
+builder.Services.AddScoped<ISelectDataService, SelectDataService>();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
